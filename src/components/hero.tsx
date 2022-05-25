c1e4af3030f80683e07cad62d2de835532c87872
ntabucejo <ntabucejo@gmail.com>
2022-05-25 20:27:52 +0800

    Fix hero

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -9,7 +9,7 @@ const Hero = () => {
         <Welcome />
         <div className="absolute bottom-0 left-0 md:left-auto md:right-0 py-4 mr-5">
           <img className="h-8 md:h-10 mr-auto md:mr-0 md:ml-auto" src={squarespace} alt="squarespace" />
-          <p className="text-white mx-3">Start your website with Squarespace today.</p>
+          <p className="text-white mx-3 text-xs">Start your website with Squarespace today.</p>
         </div>
       </div>
       <img className="h-full object-cover w-full bg-bottom brightness-50" src={hero} alt="hero" />
--- src/components/welcome.tsx
+++ src/components/welcome.tsx
@@ -5,12 +5,36 @@ const Welcome = () => {
     <div className="max-w-4xl relative md:left-1/2 md:-translate-x-1/2 space-y-2 md:space-y-6 mb-14 md:mb-0">
       <h1 className="text-2xl md:text-5xl font-bold text-white">Unsplash</h1>
       <div className="text-white text-sm md:text-lg md:font-semibold">
-        <p>The internet’s source of freely-usable images.</p>
+        <p>
+          The internet’s source of{' '}
+          <span className="underline font-light cursor-pointer text-neutral-200 hover:text-white">
+            freely-usable images.
+          </span>
+        </p>
         <p>Powered by creators everywhere.</p>
       </div>
       <div className="space-y-4 hidden md:block">
         <Search variant="hero" />
-        <p className="text-white">Trending: FlowerWallpapers,Backgrounds, Happy, Love</p>
+        <ul className="text-neutral-200 flex gap-2">
+          <li className="hover:text-white">
+            <a href="#">Trending</a>
+          </li>
+          <li className="hover:text-white">
+            <a href="#">Flower</a>
+          </li>
+          <li className="hover:text-white">
+            <a href="#">Wallpapers</a>
+          </li>
+          <li className="hover:text-white">
+            <a href="#">Backgrounds</a>
+          </li>
+          <li className="hover:text-white">
+            <a href="#">Happy</a>
+          </li>
+          <li className="hover:text-white">
+            <a href="#">Love</a>
+          </li>
+        </ul>
       </div>
     </div>
   )
