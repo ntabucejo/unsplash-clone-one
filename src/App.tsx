824ed9527e03e368745f255e2a5f99ef8dda454c
ntabucejo <ntabucejo@gmail.com>
2022-05-25 08:29:59 +0800

    Add header

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import Header from './components/header'
 import Layout from './components/layout'
 import Main from './components/main'
 
 const App = () => {
   return (
     <Layout>
+      <Header />
       <Main>section</Main>
     </Layout>
   )
